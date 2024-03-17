c8a7db562f822123462fdd693f72234e35d91a43
De Dee <91461521+HtooMy@users.noreply.github.com>
2024-03-17 10:30:21 +0630

    Update MobileNav.tsx

--- Components/MobileNav.tsx
+++ Components/MobileNav.tsx
@@ -12,12 +12,12 @@ const MobileNav = ({nav, closeNav}: Props) => {
   return (
     <div className={`fixed ${navAnimation}  transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[100000] bg-[#09101a]`}>
         <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
-            <div className='nav-link-mobile'><a href="#top">HOME</a></div>
-            <div className='nav-link-mobile'><a href="#service">SERVICES</a></div>
-            <div className='nav-link-mobile'><a href="#about">ABOUT</a></div>
-            <div className='nav-link-mobile'><a href="#project">PROJECT</a></div>
-            <div className='nav-link-mobile'><a href="#blog">BLOG</a></div>
-            <div className='nav-link-mobile'><a href="#contact">CONTACT</a></div>
+            <div onClick={closeNav} className='nav-link-mobile'><a href="#top">HOME</a></div>
+            <div onClick={closeNav} className='nav-link-mobile'><a href="#service">SERVICES</a></div>
+            <div onClick={closeNav} className='nav-link-mobile'><a href="#about">ABOUT</a></div>
+            <div onClick={closeNav} className='nav-link-mobile'><a href="#project">PROJECT</a></div>
+            <div onClick={closeNav} className='nav-link-mobile'><a href="#blog">BLOG</a></div>
+            <div onClick={closeNav} className='nav-link-mobile'><a href="#contact">CONTACT</a></div>
         </div>
         <div 
         onClick={closeNav}
