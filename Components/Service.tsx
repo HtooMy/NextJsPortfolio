d0aadf38e8ec651c2ae895593a8d77c1af80ece2
De Dee <91461521+HtooMy@users.noreply.github.com>
2024-03-17 10:21:04 +0630

    Update Service.tsx

--- Components/Service.tsx
+++ Components/Service.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Service = () => {
   return (
-    <div className="bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]">
+    <div id="service" className="bg-[#121212] pt-[4rem] md:pt-[8rem] pb-[5rem]">
       <p className="heading">
         My <span className="text-yellow-400">Services</span>
       </p>
