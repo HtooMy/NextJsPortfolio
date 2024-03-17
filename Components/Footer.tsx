05821a912da7111e85dcae9c96a0bd33ad362f9c
De Dee <91461521+HtooMy@users.noreply.github.com>
2024-03-17 10:20:25 +0630

    Update Footer.tsx

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='pt-[8rem] pb-[4rem] bg-[#02050a]'>
+    <div id="contact" className='pt-[8rem] pb-[4rem] bg-[#02050a]'>
         <div className='grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto gap-[3rem]'>
             <div className='flex items-center space-x-6'>
                 <div className='md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]'>
