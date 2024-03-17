0056cd6c061455a644c95fa05f58e85c9d70c903
De Dee <91461521+HtooMy@users.noreply.github.com>
2024-03-17 10:20:38 +0630

    Update Project.tsx

--- Components/Project.tsx
+++ Components/Project.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Project = () => {
   return (
-    <div className="bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]">
+    <div id="project" className="bg-[#02050a] pt-[4rem] md:pt-[8rem] pb-[1rem]">
       <h1 className="heading">
         Pro<span className="text-yellow-400">jects</span>
       </h1>
